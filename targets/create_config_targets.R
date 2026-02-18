1530c5d43a8bd219c1a3181d9b744fc249293d86
Kaitlyn Johnson <kaitlyn.johnson@lshtm.ac.uk>
2026-02-18 16:45:58 +0000

    update pre-commit and forecast date

--- targets/create_config_targets.R
+++ targets/create_config_targets.R
@@ -3,7 +3,7 @@ create_config_targets <- list( # nolint
   # Retrospective forecast date from last season
   tar_target(
     name = forecast_date,
-    command = "2026-02-11" # Wednesday forecast date
+    command = "2026-02-18" # Wednesday forecast date
   ),
   tar_target(
     name = reference_date,
